e18fb35d75f06ccbd86c8a390bff4e4474dbbd6f
Andrew Clemons <andrew.clemons@fluxfederation.com>
2020-05-01 16:21:16 +1200

    Revert "Don't pass `merge_when_pipeline_succeeds` to accept the merge request"
    This reverts commit 2edc59821f9c36d6d83b1fd8ed12eeaca4a23b78.

--- marge/merge_request.py
+++ marge/merge_request.py
@@ -163,6 +163,7 @@ class MergeRequest(gitlab.Resource):
             '/projects/{0.project_id}/merge_requests/{0.iid}/merge'.format(self),
             dict(
                 should_remove_source_branch=remove_branch,
+                merge_when_pipeline_succeeds=merge_when_pipeline_succeeds,
                 sha=sha or self.sha,  # if provided, ensures what is merged is what we want (or fails)
             ),
         ))
